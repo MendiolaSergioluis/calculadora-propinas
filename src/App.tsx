3e87c89e419935226aa6e7f45b84f54bf3ca8c7f
Sergioluis <mendiolasergioluis@gmail.com>
2024-06-19 18:19:27 -0400

    Contenedores principales agregados

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ console.log(menuItems)
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y consumo</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="">
+          <h2>Menú</h2>
+        </div>
+        <div className="">
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
